a58d50caf2fabfea72753f187f0d2a93f0d4ac34
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:52:15 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 09:52:14
+class Solution {
+    public boolean isAnagram(String s, String t) 
+    {
+        if(s.length()!=t.length()) return false;
+        int freq[] = new int[256];
+        for(char ch : s.toCharArray()){
+            freq[ch]++;
+        }
+        for (char ch : t.toCharArray()) {
+            freq[ch]--;
+        }
+        for (int count : freq) {
+            if (count != 0) return false;
+        }
+        return true;
+    }
+}
