af776814ac8b61aeac5701d0f62b2213a5ec1dae
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:49:23 +0530

    Create 824-NumberOfLinesToWriteString

---
+++ 824-NumberOfLinesToWriteString/824-NumberOfLinesToWriteString.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:49:23
+class Solution {
+    public int[] numberOfLines(int[] widths, String s) {
+        int lines = 1;
+        int currentWidth = 0;
+
+        for (char c : s.toCharArray()) {
+            int w = widths[c - 'a'];
+
+            if (currentWidth + w > 100) {
+                lines++;
+                currentWidth = w;
+            } else {
+                currentWidth += w;
+            }
+        }
+
+        return new int[]{lines, currentWidth};
+    }
+}
