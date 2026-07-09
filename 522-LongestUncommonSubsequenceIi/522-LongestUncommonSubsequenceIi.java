4e84724f231b6568aa736cb211ca603b774019e0
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:50:20 +0530

    Create 522-LongestUncommonSubsequenceIi

---
+++ 522-LongestUncommonSubsequenceIi/522-LongestUncommonSubsequenceIi.java
@@ -0,0 +1,37 @@
+// Last updated: 09/07/2026, 09:50:20
+import java.util.Arrays;
+
+class Solution {
+    
+    public int findLUSlength(String[] strs) {
+        Arrays.sort(strs, (a, b) -> b.length() - a.length());
+
+        for (int i = 0; i < strs.length; i++) {
+            boolean uncommon = true;
+
+            for (int j = 0; j < strs.length; j++) {
+                if (i == j) continue;
+
+                if (isSubsequence(strs[i], strs[j])) {
+                    uncommon = false;
+                    break;
+                }
+            }
+
+            if (uncommon) return strs[i].length();
+        }
+
+        return -1;
+    }
+
+    private boolean isSubsequence(String a, String b) {
+        int i = 0, j = 0;
+
+        while (i < a.length() && j < b.length()) {
+            if (a.charAt(i) == b.charAt(j)) i++;
+            j++;
+        }
+
+        return i == a.length();
+    }
+}
