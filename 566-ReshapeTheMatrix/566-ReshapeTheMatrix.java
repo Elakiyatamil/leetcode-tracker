11f479d8f2663e7424374f0d14916895b6d993be
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:50:08 +0530

    Create 566-ReshapeTheMatrix

---
+++ 566-ReshapeTheMatrix/566-ReshapeTheMatrix.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 09:50:07
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;
+        }
+        int[][] result = new int[r][c];
+        for (int i = 0; i < m * n; i++) {
+            result[i / c][i % c] = mat[i / n][i % n];
+        }
+        return result;
+    }
+}
