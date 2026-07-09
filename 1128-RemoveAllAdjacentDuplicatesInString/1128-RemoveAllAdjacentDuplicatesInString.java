a254dbf3fac95aa5f33b59689e5af063a23892b3
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:48:23 +0530

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ 1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:48:22
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop(); // remove duplicate
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
