8290eccf9079e584f722c89f1e468f5a91773a2a
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:49:53 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:49:52
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int duplicate = -1;
+        int missing = -1;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+                duplicate = i;
+            } else if (freq[i] == 0) {
+                missing = i;
+            }
+        }
+        return new int[]{duplicate, missing}; 
+    }
+}
