85ece3510bd1a06a766bca7fc0cbac71fa9483ea
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:50:22 +0530

    Create 521-LongestUncommonSubsequenceI

---
+++ 521-LongestUncommonSubsequenceI/521-LongestUncommonSubsequenceI.java
@@ -0,0 +1,9 @@
+// Last updated: 09/07/2026, 09:50:21
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+    }
+}
