510788cb469128ad18244319c6ba7dfd5e1858d4
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:52:10 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:52:09
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i=0;i<n;i++)
+        {
+            int h=n-i;
+            if(citations[i]>=h)
+            {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
