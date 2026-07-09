12f85612f9ee52b0120e7c05f05301e4b20d2aed
Elakiyatamil <tamilelakiya004@gmail.com>
2026-07-09 09:48:59 +0530

    Create 961-LongPressedName

---
+++ 961-LongPressedName/961-LongPressedName.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:48:59
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0, j = 0;
+
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } 
+            else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++; // long press case
+            } 
+            else {
+                return false;
+            }
+        }
+
+        return i == name.length();
+    }
+}
